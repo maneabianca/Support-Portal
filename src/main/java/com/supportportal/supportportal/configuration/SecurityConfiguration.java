51be2f67579ce192cfef9ae11439f707316a20d2
Bianca <manea.bianca9@yahoo.ro>
2025-06-19 14:16:04 +0300

    Adding the login functionality

--- src/main/java/com/supportportal/supportportal/configuration/SecurityConfiguration.java
+++ src/main/java/com/supportportal/supportportal/configuration/SecurityConfiguration.java
@@ -98,7 +98,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
--- src/main/java/com/supportportal/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/supportportal/resource/UserResource.java
@@ -1,14 +1,20 @@
 package com.supportportal.supportportal.resource;
 
+import com.supportportal.supportportal.constant.SecurityConstant;
 import com.supportportal.supportportal.domain.User;
+import com.supportportal.supportportal.domain.UserPrincipal;
 import com.supportportal.supportportal.exception.ExceptionHandling;
 import com.supportportal.supportportal.exception.domain.EmailExistException;
 import com.supportportal.supportportal.exception.domain.UserNotFoundException;
 import com.supportportal.supportportal.exception.domain.UsernameExistException;
 import com.supportportal.supportportal.service.UserService;
+import com.supportportal.supportportal.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,16 +22,57 @@ import org.springframework.web.bind.annotation.*;
 public class UserResource extends ExceptionHandling {
 
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserResource(UserService userService) {
+    public UserResource(UserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
+
+
+
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, EmailExistException, UsernameExistException {
         User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
         return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+
+        authenticate(user.getUsername(), user.getPassword());
+
+        User loginUser = userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    /**
+     * Generates an HTTP header containing a JWT for a given authenticated user
+     * @param userPrincipal the authenticated user
+     * @return an HTTPHeader object with the JWT included
+     */
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(SecurityConstant.JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(userPrincipal));
+        return headers;
+    }
+
+    /**
+     * Authenticates using the provided username and password.
+     * This method does not return a value; it either successfully authenticates the user or
+     * throws an exception if the authentication fails.
+     * @param username the user's login name
+     * @param password the user's password
+     */
+    private void authenticate(String username, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
     }
 
 }
